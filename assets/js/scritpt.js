26450a768f0410f74bdfcd2d0b81bd95d8e91bfe
elielasf <elielasf@hotmail.com>
2022-06-01 15:19:20 -0300

    Ajustes feitos

--- assets/js/scritpt.js
+++ assets/js/scritpt.js
@@ -11,12 +11,14 @@ const formatarMoedaTotal = new Intl.NumberFormat("pt-br", {
     minimumFractionDigits: 2,
 });
 
+desenhaTabela()
+
 //Impede o usuário de colar algo no campo valor.
 const inputValor = document.querySelector("#inpt-valor");
 inputValor.addEventListener("paste", function(x){
     x.preventDefault()
 });
-
+ 
 function desenhaTabela() {
 
     let total = 0;
@@ -37,17 +39,18 @@ function desenhaTabela() {
         
         let valor = parseFloat(extrato[dados].valor.replace(/[^0-9]/g, ""));
         
-    if (extrato[dados].compraVenda) {
-        total -= valor;
-    } else {
-        total += valor;
-    }
+        if (extrato[dados].compraVenda) {
+            total -= valor;
+        } else {
+            total += valor;
+        }
 
-    document.querySelector('table.tabela-extrato tbody').innerHTML += `
-        <tr class="conteudo-dinamico">
-            <td class="nome-mercadoria">${ (extrato[dados].compraVenda ? '-' : '+')} &nbsp; ${extrato[dados].nome}</td>
-            <td class="preco-mercadoria">R$ ${extrato[dados].valor}</td>
-        </tr>`};
+        document.querySelector('table.tabela-extrato tbody').innerHTML += `
+            <tr class="conteudo-dinamico">
+                <td class="nome-mercadoria">${ (extrato[dados].compraVenda ? '-' : '+')} &nbsp; ${extrato[dados].nome}</td>
+                <td class="preco-mercadoria">${extrato[dados].valor}</td>
+            </tr>`
+    };
     
     if (extrato.length > 0) {
       
@@ -66,7 +69,7 @@ function desenhaTabela() {
                     <td style="border:none;"> </td> 
                     <td  class="despesa-lucro">[Lucro]</td> 
                 </tr>`
-        } else {
+        } else if (total < 0) {
             document.querySelector('table.tabela-extrato tbody').innerHTML += `
                 <tr class="conteudo-dinamico"> 
                     <td style="border:none;"> </td> 
@@ -77,7 +80,7 @@ function desenhaTabela() {
     }
 }
 
-function limparDados(p) {
+function limparDados() {
     
     if (extrato.length > 0 && window.confirm("Deseja remover todas as informações?")) {
         for (element of document.querySelectorAll(".conteudo-dinamico")) {
@@ -94,68 +97,24 @@ function limparDados(p) {
 function testaForm(e) {
     e.preventDefault();
     
-    var extratoRaw = localStorage.getItem('extrato')
-
-    if (extratoRaw != null) {
-        var extrato = JSON.parse(extratoRaw)
-    } else {
-        var extrato = [];
-    }
-    
     extrato.push({
         compraVenda: (e.target.elements['compra-venda'].value == 'compra'),
         nome: e.target.elements['inpt-nome'].value,
         valor: e.target.elements['inpt-valor'].value
     }) 
-
     localStorage.setItem('extrato', JSON.stringify(extrato))
-    window.location.href = ''
+    desenhaTabela()
 }
 
 function cadastroTransacoes() {
     document.getElementById("compra-venda").focus();
 } 
 
-//Máscara para formatar a moeda ao digitar no input. Máscara retirada do código de um aluno que já fez o projeto; https://github.com/EdiJunior88/NewTab_Academy_Projeto_Individual_JavaScript/blob/main/javascript/index.js
-function formatarMoeda(objTextBox, SeparadorMilesimo, SeparadorDecimal, e){  
-    var sep = 0;  
-    var key = '';  
-    var i = j = 0;  
-    var len = len2 = 0;  
-    var strCheck = '0123456789';  
-    var aux = aux2 = '';  
-    var whichCode = (window.Event) ? e.which : e.keyCode;  
-    if (whichCode == 13 || whichCode == 8) return true;  
-    key = String.fromCharCode(whichCode); 
-    if (strCheck.indexOf(key) == -1) return false; 
-    len = objTextBox.value.length;  
-    for(i = 0; i < len; i++)  
-        if ((objTextBox.value.charAt(i) != '0') && (objTextBox.value.charAt(i) != SeparadorDecimal)) break;  
-    aux = '';  
-    for(; i < len; i++)  
-        if (strCheck.indexOf(objTextBox.value.charAt(i))!=-1) aux += objTextBox.value.charAt(i);  
-    aux += key;  
-    len = aux.length;  
-    if (len == 0) objTextBox.value = '';  
-    if (len == 1) objTextBox.value = '0'+ SeparadorDecimal + '0' + aux;  
-    if (len == 2) objTextBox.value = '0'+ SeparadorDecimal + aux;  
-    if (len > 2) {  
-        aux2 = '';  
-        for (j = 0, i = len - 3; i >= 0; i--) {  
-            if (j == 3) {  
-                aux2 += SeparadorMilesimo;  
-                j = 0;  
-            }  
-            aux2 += aux.charAt(i);  
-            j++;  
-        }  
-        objTextBox.value = '';  
-        len2 = aux2.length;  
-        for (i = len2 - 1; i >= 0; i--)  
-        objTextBox.value += aux2.charAt(i);  
-        objTextBox.value += SeparadorDecimal + aux.substr(len - 2, len);  
-    }  
-    return false;  
-}
-desenhaTabela()
-
+jQuery(function() {
+        
+    jQuery("#inpt-valor").maskMoney({
+        prefix:'R$',
+        thousands:'.', 
+        decimal:','
+    })
+});
